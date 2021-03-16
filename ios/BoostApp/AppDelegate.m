384f401bd9cfcef6eec88b90e134d02697fd5dac
yahyamokaied <yahyamokaied@gmail.com>
2021-03-16 09:00:41 +0100

    update packages

--- ios/BoostApp/AppDelegate.m
+++ ios/BoostApp/AppDelegate.m
@@ -5,6 +5,9 @@
 #import <React/RCTRootView.h>
 
 #import "RNBootSplash.h"
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 #import <CodePush/CodePush.h>
 #import <UMCore/UMModuleRegistry.h>
@@ -42,6 +45,11 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
